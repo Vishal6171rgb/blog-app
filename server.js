ae1264fdeb2fefd51227c554ba2e8e4a7407974f
Vishal <devadigavishal28@gmail.com>
2025-06-15 12:10:25 +0530

    Initial commit

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ app.delete("/posts/:id", (req, res) => {
 });
 
 // Start the server on port 5000
-const PORT = 5000;
+const PORT =process.env.PORT||5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
